4334035625ce84162e3d551d4634f3be0e8f5793
Sascha Weidner <=>
2019-07-23 11:50:49 +0200

    Initialize

---
+++ metadata.php
@@ -0,0 +1,32 @@
+<?php
+
+$sMetadataVersion = '2.1';
+$aModule = [
+    'id'          => 'devutils-logs',
+    'title'       => '[devutils] logs',
+    'description' => 'Logs parser for oxid eshop.<br/>add this code to your config.inc.php to save webserver errors into log/error.log<br><textarea cols="80" rows="3">ini_set(\'error_reporting\',24567); 
+ini_set(\'log_errors\',1);
+ini_set(\'error_log\',dirname(__FILE__).\'/log/error.log\');</textarea><hr/>access token for chrome extension: ' . md5($_SERVER["DOCUMENT_ROOT"]),
+    'version'     => '1.0.0',
+    'author'      => 'Marat Bedoev',
+    'email'       => 'm@marat.ws',
+    'url'         => 'https://github.com/OXIDprojects/devutils-oxhackathon19',
+    'extend'      => [
+        \OxidEsales\Eshop\Application\Controller\StartController::class => OxidCommunity\DevutilsLogs\Controller\StartController::class
+    ],
+    'controllers' => [
+        'dev_logs' => OxidCommunity\DevutilsLogs\Controller\Admin\Logs::class
+    ],
+    'templates'   => [
+        'dev_logs.tpl' => 'oxcom/devutils-logs/views/admin/dev_logs.tpl'
+    ],
+    'settings'    => [
+        [
+            'group'    => 'Dev',
+            'name'     => 's_Dev_serverLogPath',
+            'type'     => 'str',
+            'position' => 0,
+            'value'    => 'log/error.log'
+        ]
+    ]
+];
---
+++ views/admin/de/module_options.php
@@ -0,0 +1,8 @@
+<?php
+
+$sLangName = 'Deutsch';
+$aLang = array(
+    'charset'                         => 'UTF-8',
+    'dev_logs'                        => 'Logs',
+    'SHOP_MODULE_s_Dev_serverLogPath' => 'Pfad zum apache webserver log (relativ vom shop root oder absolut)'
+);
